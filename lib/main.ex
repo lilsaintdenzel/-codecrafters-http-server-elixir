c57f170301f5a6d62e1d7e757b747eb6a5481bf8
progressorish <progressorish@heales.com>
2025-07-18 16:09:58 +0100

    [any message]

--- lib/main.ex
+++ lib/main.ex
@@ -13,8 +13,8 @@ defmodule Server do
     #
     # # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # # ensures that we don't run into 'Address already in use' errors
-    # {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
-    # {:ok, _client} = :gen_tcp.accept(socket)
+    {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
+    {:ok, _client} = :gen_tcp.accept(socket)
   end
 end
 
